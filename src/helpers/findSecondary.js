5da0023c85e750ba236a850efe8ef3c8265a0739
Andrew Wallis <andrewwallis@andrews-air.home>
2026-03-12 09:46:30 +0000

    Reverting algorithm

--- src/helpers/findSecondary.js
+++ src/helpers/findSecondary.js
@@ -2,20 +2,10 @@ import isObj from "../utils/isObj";
 
 function findSecondary(primary, secondaryFonts) {
 
-  const blacklist = ["Lato", "Noto Sans", "Nunito Sans", "Open Sans"];
-  const filteredFonts = secondaryFonts.filter(font => !blacklist.includes(font.name));
-
   let secondary = {};
 
   if(primary.family) {
-    let familyFonts = filteredFonts.filter(font => font.family === primary.family);
-
-    if(primary.superclass === "Sans") {
-      familyFonts = familyFonts.filter(font => font.superclass === "Serif");
-    } else {
-      familyFonts = familyFonts.filter(font => font.superclass === "Sans");
-    }
-
+    let familyFonts = secondaryFonts.filter(font => font.family === primary.family);
     familyFonts.sort((a, b) => Number(b.Rating) - Number(a.Rating));
     secondary = familyFonts.length ? familyFonts[0] : {};
   }
@@ -24,7 +14,7 @@ function findSecondary(primary, secondaryFonts) {
 
     for (let i = 0; i <= 0.5; ) {
       
-      let xHeightFonts = filteredFonts.filter(font => font.xHeightAbs === i);
+      let xHeightFonts = secondaryFonts.filter(font => font.xHeightAbs === i);
 
       if(primary.superclass === "Sans") {
         xHeightFonts = xHeightFonts.filter(font => font.superclass === "Serif");
@@ -33,23 +23,13 @@ function findSecondary(primary, secondaryFonts) {
       }
   
       if(xHeightFonts.length) {
-        
-/*         let distributionFonts = {};
-
-        if(primary.distribution === "Adobe") {
-          distributionFonts = xHeightFonts.filter(font => font.distribution === "Adobe");
-        } else {
-          distributionFonts = xHeightFonts.filter(font => font.distribution !== "Adobe");
-        }
-
-        xHeightFonts = distributionFonts.length ? distributionFonts : xHeightFonts; */
 
         let vibeFonts = xHeightFonts.filter(font => font.vibe === primary.vibe);
         let neutralFonts = xHeightFonts.filter(font => font.vibe === "Neutral");
         let returnFonts = vibeFonts.length ? vibeFonts : neutralFonts;
 
         returnFonts = returnFonts.sort((a, b) => Number(b.Rating) - Number(a.Rating));
-        secondary = returnFonts.length ? returnFonts[0] : filteredFonts[0];
+        secondary = returnFonts.length ? returnFonts[0] : secondaryFonts[0];
         break;
       } else {
         i += 0.02;
